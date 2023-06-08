3757633107f694ee78399bbbb7deaf96c323ba29
nmartin5 <nicolas.martinelli@odigo.com>
2023-06-08 17:19:05 +0200

    loading basic image, user input for number of tiles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use image::GenericImageView;
+
 fn main() {
     println!("Hello, world!");
+
+    loop {
+        let mut input = String::new();
+        println!("Enter an even number: ");
+        input.clear();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let number = match input.trim().parse::<i32>() {
+            Ok(i) => i,
+            Err(..) => {
+                println!("This was not an integer: {}", input.trim());
+                continue;
+            }
+        };
+
+        if number % 2 == 0 {
+            break;
+        } else {
+            println!("This was not an even number: {}", number);
+        }
+    }
+
+    let image = image::open("to_mosaic/image.jpg").expect("File not found!");
+
+    let (w, h) = image.dimensions();
+    println!("Image dimensions: {} x {}", w, h);
+
+    println!("There are {} pixels", image.pixels().count());
 }
